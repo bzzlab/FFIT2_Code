7a5be410af06c26c0ac3debc44e98ad59a6baf73
dgaravaldi <daniel.garavaldi@gmail.com>
2024-03-19 07:41:25 +0100

    02/Demo+Exerc

---
+++ 02/00_Demo/02/client/script/fetchData.js
@@ -0,0 +1,10 @@
+/**
+ * asynchronous function for fetching data
+ * @returns {Promise<any>}
+ */
+const SERVER = "http://localhost:3000";
+//??
+//??
+    // waits until the request completes...
+//??
+//??
