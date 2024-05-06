03e9df05c4f0e0fc2e6e7198c52ad9cafae4a657
Daniel Garavaldi <daniel.garavaldi@gmail.com>
2024-05-06 22:52:00 +0200

    03a - socket.io

---
+++ 03a/00_Demo/index.js
@@ -0,0 +1,30 @@
+//Import express, http and socket.io
+//??
+//??
+//??
+//assign express to app
+//??
+//Request client with /
+app.get('/', function (req, res) {
+    //send index.html to the client
+//??
+});
+//set http-server and listen
+//??
+//set port to 5000
+//??
+//??
+console.log(`server running on port ${port}!`)
+//set socket io
+//??
+//connect to client ...
+//??
+    //emit message to client
+//??
+//??
+//??
+    //listen to message from client
+//??
+//??
+//??
+//??
